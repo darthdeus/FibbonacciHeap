8c5072aa6add9fb74c6c07bf797c0fab69787cf9
Jakub Arnold <darthdeus@gmail.com>
2017-11-17 18:17:47 +0100

    Funkcni list

--- Program.cs
+++ Program.cs
@@ -3,84 +3,161 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace Halda {
     //// TODO: udelat z toho structuru
-    //public class DoubleLinkedList {
-    //    public Node First;
-    //    public Node Last;
-    //    public bool IsEmpty => First == null;
+    public class DoubleLinkedList {
+        public Node First;
+        public Node Last;
+        public bool IsEmpty => First == null;
 
-    //    public void Insert(Node node) {
-    //        if (First == null) {
-    //            First = node;
-    //            Last = node;
-    //        } else {
-    //            Debug.Assert(Last != null);
-    //            Debug.Assert(Last.Right == null);
-    //            Debug.Assert(node.Left == null);
-    //            Debug.Assert(node.Right == null);
+        public void Insert(Node node) {
+            if (First == null) {
+                First = node;
+                Last = node;
+            } else {
+                Debug.Assert(Last != null);
+                Debug.Assert(Last.Right == null);
+                Debug.Assert(node.Left == null);
+                Debug.Assert(node.Right == null);
 
-    //            Last.Right = node;
-    //            Last = node;
-    //        }
-    //    }
-
-    //    public Node Dequeue() {
-    //        Debug.Assert(First != null);
-    //        Debug.Assert(First.Left == null);
-
-    //        var retval = First;
-
-    //        First = First.Right;
-    //        First.Left = null;
-
-    //        return retval;
-    //    }
-
-    //    public void Merge(DoubleLinkedList other) {
-    //        Debug.Assert(Last != null);
-    //        Debug.Assert(other.First != null);
-    //        Debug.Assert(Last.Right == null);
-    //        Debug.Assert(other.First.Left == null);
-
-    //        Last.Right = other.First;
-    //        other.First.Left = Last;
-    //    }
-    //}
-
-    public class Link<T> {
-        public T Item;
-        public Link<T> Next;
-
-        public Link(T item) {
-            Item = item;
+                Last.Right = node;
+                Last = node;
+            }
         }
 
-        public Link(T item, Link<T> next) {
-            Item = item;
-            Next = next;
+        public Node Dequeue() {
+            Debug.Assert(First != null);
+            Debug.Assert(First.Left == null);
+
+            var retval = First;
+
+            First = First.Right;
+            First.Left = null;
+
+            return retval;
+        }
+
+        public void Merge(DoubleLinkedList other) {
+            Debug.Assert(Last != null);
+            Debug.Assert(other.First != null);
+            Debug.Assert(Last.Right == null);
+            Debug.Assert(other.First.Left == null);
+
+            Last.Right = other.First;
+            other.First.Left = Last;
         }
     }
 
+    [DebuggerDisplay("{Value}")]
     public class Node {
         public int Value;
         public int Rank = 0;
         public bool Marked = false;
         public bool IsRoot => Parent == null;
 
+        public Node Children;
         public Node Parent;
         public Node Right;
         public Node Left;
 
-        public Node Children;
-
         public Node(int value) {
             Value = value;
+            Left = this;
+            Right = this;
         }
 
         public static Node Root(int value) {
             return new Node(value);
+        }
+
+        public void Merge(Node other) {
+            var x = this.Right;
+            var y = other.Right;
+
+            this.Right = other;
+            other.Left = this;
+
+            x.Left = y;
+            y.Right = x;
+        }
+
+        public Node Remove() {
+            if (this.Right == this && this.Left == this) {
+                return null;
+            }
+
+
+            this.Left.Right = Right;
+            this.Right.Left = Left;
+
+            var ret = Left;
+
+            Left = null;
+            Right = null;
+
+            return ret;
+        }
+
+        private static int count = 0;
+
+        public void PrintDotgraph() {
+            using (var file = new StreamWriter($"graph{count}.dot")) {
+                file.WriteLine("digraph G {");
+
+                var visited = new HashSet<int>();
+                var stack = new Stack<Node>();
+
+                stack.Push(this);
+
+                while (stack.Count > 0) {
+                    var node = stack.Pop();
+
+                    if (visited.Contains(node.Value)) continue;
+                    visited.Add(node.Value);
+
+                    file.WriteLine($"\"{node.Value}\" [fillcolor = pink, style=filled]");
+
+                    if (node.Parent != null) {
+                        stack.Push(node.Parent);
+                        file.WriteLine($"{node.Value} -> {node.Parent.Value} [color=\"yellow\"]");
+                    }
+
+                    if (node.Children != null) {
+                        stack.Push(node.Children);
+                        file.WriteLine($"{node.Value} -> {node.Children.Value} [color=\"green\"]");
+                    }
+
+                    if (node.Right != null) {
+                        stack.Push(node.Right);
+                        file.WriteLine($"{node.Value} -> {node.Right.Value} [color=\"red\"]");
+                    }
+                    if (node.Left != null) {
+                        stack.Push(node.Left);
+                        file.WriteLine($"{node.Value} -> {node.Left.Value} [color=\"blue\"]");
+                    }
+                }
+
+                file.WriteLine("}");
+            }
+
+            Process.Start("dot", $"-Tpng -o graph{count}.png graph{count}.dot");
+            count++;
+        }
+
+        private string DebugString() {
+            var builder = new StringBuilder();
+
+            if (Left != null && Right != null) {
+                builder.Append($"{Left.Value} <-> {Value} <-> {Right.Value}");
+            } else {
+                builder.Append($"{Value}");
+            }
+
+
+            return builder.ToString();
         }
     }
 
@@ -88,25 +165,22 @@ namespace Halda {
         private Node _trees;
         private Node _minNode = null;
         private int _count = 0;
-        private FibHeap _nextHeap;
 
         public void Insert(int value) {
-            var node = Node.Root(value);
+            InsertNode(Node.Root(value));
+        }
 
-            InsertNode(node);
+        public void InsertNode(Node node) {
+            Debug.Assert(node.Left == node);
+            Debug.Assert(node.Right == node);
+
+            _count++;
+            _trees.Merge(node);
+
 
             if (_minNode == null || _minNode.Value > node.Value) {
                 _minNode = node;
             }
-        }
-
-        public void InsertNode(Node node) {
-            Debug.Assert(node.Left == null);
-            Debug.Assert(node.Right == null);
-            Debug.Assert(_trees.Left == null);
-
-            node.Right = _trees;
-            _trees.Left = node;
         }
 
         public void Merge(FibHeap other) {
@@ -115,19 +189,11 @@ namespace Halda {
 
             _count += other._count;
 
-            _trees.Right = other._trees;
-            other._trees.Left = _trees;
+            _trees.Merge(other._trees);
 
-            //_trees.Right = other._trees.Left;
-            //other._trees.Left.Right = _trees.Right;
-
-            //_trees.
-
-            //other._trees.Left = _trees;
-            //other._trees
-
-            //_trees.Merge(other._trees);
-            //other._trees = null;
+            if (other._minNode.Value < _minNode.Value) {
+                _minNode = other._minNode;
+            }
         }
 
         public Node Min() {
@@ -139,13 +205,16 @@ namespace Halda {
             int retval = extracted.Value;
             _count--;
 
-            var node = extracted.Children;
+            _trees = extracted.Remove();
 
-            while (node != null) {
+            var node = extracted.Children;
+            var first = node;
+
+            do {
                 node.Parent = null;
                 InsertNode(node);
                 node = node.Right;
-            }
+            } while (node != first);
 
             Consolidate();
 
@@ -161,41 +230,37 @@ namespace Halda {
         }
 
         private void Consolidate() {
-            var buckets = new Link<Node>[(int)Math.Round(Math.Log(_count))];
+            var buckets = new Node[(int) Math.Round(Math.Log(_count))];
 
-            var node = _trees;
+            do {
+                var node = _trees;
+                _trees = _trees.Remove();
 
-            while (node != null) {
                 if (buckets[node.Rank] == null) {
-                    buckets[node.Rank] = new Link<Node>(node);
+                    buckets[node.Rank] = node;
                 } else {
-                    buckets[node.Rank] = new Link<Node>(node, buckets[node.Rank]);
+                    buckets[node.Rank].Merge(node);
                 }
-                node = node.Right;
-            }
+                
+            } while (_trees != null);
 
             for (int i = 0; i < buckets.Length; i++) {
                 while (buckets[i] != null) {
-                    var item = buckets[i];
+                    var item1 = buckets[i];
+                    buckets[i] = buckets[i].Remove();
 
-                    if (item.Next != null) {
-                        
-                    }
-                }
-            }
+                    if (buckets[i] != null) {
+                        var item2 = buckets[i];
+                        buckets[i] = buckets[i].Remove();
 
-            foreach (var list in buckets) {
-
-                while (!list.IsEmpty) {
-                    var first = list.Dequeue();
-
-                    if (list.IsEmpty) {
-                        _trees.Insert(first);
-                        break;
+                        item1.Merge(item2);
+                        buckets[i+1].Merge(item1);
                     } else {
-                        var second = list.Dequeue();
-
-                        
+                        if (_trees == null) {
+                            _trees = item1;
+                        } else {
+                            _trees.Merge(item1);
+                        }
                     }
                 }
             }
@@ -215,7 +280,8 @@ namespace Halda {
                 x.Marked = false;
                 // TODO: todle ceknout
 
-                _trees.Insert(x);
+                // TODO: zakomentovano!!!!!!!!!!!!!!!!!
+                //_trees.Insert(x);
 
                 if (o.Marked) {
                     // TODO: fuj rekurze
@@ -233,7 +299,29 @@ namespace Halda {
 
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var n1 = new Node(2);
+            n1.Merge(new Node(3));
+            n1.Merge(new Node(4));
+            n1.Merge(new Node(5));
+            n1.Merge(new Node(6));
+
+            n1.PrintDotgraph();
+
+            n1.Remove().PrintDotgraph();
+            n1.PrintDotgraph();
+
+
+            //n1.Merge(new Node(4));
+
+            //var n2 = new Node(5);
+            //n2.Merge(new Node(6));
+
+            //n1.Merge(n2);
+
+            //n1.PrintDotgraph();
+
+            //n1.Remove().PrintDotgraph();
+            //n1.PrintDotgraph();
         }
     }
 
